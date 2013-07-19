9a5b0410194a6df85fa7f84b05b9e181d095344d
raphaelstary <raphael.stary@gmail.com>
2013-07-19 17:06:53 +0200

    add basic locking mechanism to dynamic site

--- client/js/DynamicViewModel.js
+++ client/js/DynamicViewModel.js
@@ -1,10 +1,25 @@
 define(['lib/knockout'], function (ko) {
-    function DynamicViewModel(inputIds) {
+    function DynamicViewModel(disabledPostfix, selectedPostfix, inputIds) {
+        this.disabledPostfix = disabledPostfix;
         var self = this;
         inputIds.forEach(function (inputId) {
             self[inputId] = ko.observable();
+            self[inputId + disabledPostfix] = ko.observable(false);
+            self[inputId + selectedPostfix] = ko.observable(false);
         });
     }
+
+    DynamicViewModel.prototype.update = function (fieldId, value) {
+        this[fieldId](value);
+    };
+
+    DynamicViewModel.prototype.lock = function (fieldId) {
+        this[fieldId + this.disabledPostfix](true);
+    };
+
+    DynamicViewModel.prototype.unlock = function (fieldId) {
+        this[fieldId + this.disabledPostfix](false);
+    };
 
     return DynamicViewModel;
 });
--- client/js/main.js
+++ client/js/main.js
@@ -1,101 +1,142 @@
 require(['lib/knockout', 'Connector', 'Brain', 'History', 'Messenger', 'MergeConstant', 'HistoryConstant',
     'NotificationConstant', 'ConfigViewModel', 'DynamicViewModel', 'utils/getValues', 'utils/parseUrlParams',
-    'utils/orIfUndefined', 'UrlJuggler', 'lib/domReady'],
-    function (ko, Connector, Brain, History, Messenger, MergeConstant, HistoryConstant, NotificationConstant,
-              ConfigViewModel, DynamicViewModel, getValues, parseUrlParams, orIfUndefined, UrlJuggler) {
+    'utils/orIfUndefined', 'UrlJuggler', 'lib/domReady'], function (ko, Connector, Brain, History, Messenger, MergeConstant, HistoryConstant, NotificationConstant, ConfigViewModel, DynamicViewModel, getValues, parseUrlParams, orIfUndefined, UrlJuggler) {
 
-        var INPUT_PREFIX = 'input';
-        var URL = location.origin;
+    var INPUT_PREFIX = 'input';
+    var INPUT_DISABLED_POSTFIX = 'Disabled';
+    var INPUT_SELECTED_POSTFIX = 'Selected';
 
-        var connector = new Connector();
-        var brain = new Brain();
-        var history = new History();
-        var messenger = new Messenger();
-        var urlJuggler = new UrlJuggler(location.pathname, window.history.pushState.bind(window.history));
-        var urlParams = parseUrlParams(location.search);
+    var URL = location.origin;
 
-        var configView = new ConfigViewModel(getValues(MergeConstant), getValues(HistoryConstant),
-            getValues(NotificationConstant),
-            urlParams['merge'], urlParams['history'], urlParams['notification'], urlParams['user']);
+    var connector = new Connector();
+    var brain = new Brain();
+    var history = new History();
+    var messenger = new Messenger();
+    var urlJuggler = new UrlJuggler(location.pathname, window.history.pushState.bind(window.history));
+    var urlParams = parseUrlParams(location.search);
 
-        var mergeStrategy = orIfUndefined.call(urlParams['merge'], MergeConstant.PLAIN);
-        var historyStrategy = orIfUndefined.call(urlParams['history'], HistoryConstant.BY_TIME);
-        var notificationStrategy = orIfUndefined.call(urlParams['notification'], NotificationConstant.BUBBLE);
+    var configView = new ConfigViewModel(getValues(MergeConstant), getValues(HistoryConstant),
+        getValues(NotificationConstant),
+        urlParams['merge'], urlParams['history'], urlParams['notification'], urlParams['user']);
 
-        var userName = urlParams['user'];
+    var mergeStrategy = orIfUndefined.call(urlParams['merge'], MergeConstant.PLAIN);
+    var historyStrategy = orIfUndefined.call(urlParams['history'], HistoryConstant.BY_TIME);
+    var notificationStrategy = orIfUndefined.call(urlParams['notification'], NotificationConstant.BUBBLE);
 
-        var inputIds = [];
-        var inputs = document.querySelectorAll('input[id*=' + INPUT_PREFIX + ']');
-        for (var i = 0; i < inputs.length; i++) {
-            inputIds.push(inputs[i].id);
-        }
+    var userName = urlParams['user'];
 
-        var view = new DynamicViewModel(inputIds);
+    var inputIds = [];
+    var inputs = document.querySelectorAll('input[id*=' + INPUT_PREFIX + ']');
+    for (var i = 0; i < inputs.length; i++) {
+        inputIds.push(inputs[i].id);
+    }
 
-        ko.applyBindings(configView, document.getElementById('config'));
-        ko.applyBindings(view, document.getElementById('mainView'));
+    var view = new DynamicViewModel(INPUT_DISABLED_POSTFIX, INPUT_SELECTED_POSTFIX, inputIds);
 
-        var mergeSubscription = configView.merge.subscribe(function (newVal) {
-            console.log('merge update: ' + newVal);
+    ko.applyBindings(configView, document.getElementById('config'));
+    ko.applyBindings(view, document.getElementById('mainView'));
 
-            urlParams['merge'] = newVal;
-            urlJuggler.updateParams(urlParams);
-        });
+    var mergeSubscription = configView.merge.subscribe(function (newVal) {
+        console.log('merge update: ' + newVal);
 
-        var historySubscription = configView.history.subscribe(function (newVal) {
-            console.log('history update: ' + newVal);
-
-            urlParams['history'] = newVal;
-            urlJuggler.updateParams(urlParams);
-        });
-
-        var notificationSubscription = configView.notification.subscribe(function (newVal) {
-            console.log('notify update: ' + newVal);
-
-            urlParams['notification'] = newVal;
-            urlJuggler.updateParams(urlParams);
-        });
-
-        var userSubscription = configView.user.subscribe(function (newVal) {
-            console.log('user update: ' + newVal);
-
-            urlParams['user'] = newVal;
-            urlJuggler.updateParams(urlParams);
-        });
-
-        var subscriptionDict = {};
-        Object.getOwnPropertyNames(view).forEach(function (key) {
-            if (key.indexOf('input') !== -1) {
-                subscriptionDict[key] = view[key].subscribe(function (newVal) {
-
-                    console.log('update from: ' + key + ' with value: ' + newVal);
-
-                    var data = {
-                        client: brain.clientId,
-                        field: key,
-                        value: newVal
-                    };
-
-                    history.add(data);
-                    connector.send(data);
-                });
-            }
-        });
-
-        connector.connect(URL);
-
-        connector.socket.on('info', function (data) {
-            console.log('new socket info with data: ' + data);
-
-            brain.register(data);
-        });
-
-        connector.socket.on('update', function (data) {
-            console.log('new socket update with data: ' + data);
-
-            history.add(data);
-
-            view[data.field](data.value);
-        });
-
+        urlParams['merge'] = newVal;
+        urlJuggler.updateParams(urlParams);
     });
+
+    var historySubscription = configView.history.subscribe(function (newVal) {
+        console.log('history update: ' + newVal);
+
+        urlParams['history'] = newVal;
+        urlJuggler.updateParams(urlParams);
+    });
+
+    var notificationSubscription = configView.notification.subscribe(function (newVal) {
+        console.log('notify update: ' + newVal);
+
+        urlParams['notification'] = newVal;
+        urlJuggler.updateParams(urlParams);
+    });
+
+    var userSubscription = configView.user.subscribe(function (newVal) {
+        console.log('user update: ' + newVal);
+
+        urlParams['user'] = newVal;
+        urlJuggler.updateParams(urlParams);
+    });
+
+    var isInputVar = function (key) {
+        return key.toUpperCase().indexOf(INPUT_PREFIX.toUpperCase()) !== -1 &&
+            key.toUpperCase().indexOf(INPUT_DISABLED_POSTFIX.toUpperCase()) === -1 &&
+            key.toUpperCase().indexOf(INPUT_SELECTED_POSTFIX.toUpperCase()) === -1
+    };
+
+    var isSelectVar = function (key) {
+        return key.toUpperCase().indexOf(INPUT_PREFIX.toUpperCase()) !== -1 &&
+            key.toUpperCase().indexOf(INPUT_DISABLED_POSTFIX.toUpperCase()) === -1 &&
+            key.toUpperCase().indexOf(INPUT_SELECTED_POSTFIX.toUpperCase()) !== -1;
+    };
+
+    var subscriptionDict = {};
+    Object.getOwnPropertyNames(view).forEach(function (key) {
+
+        if (isInputVar(key)) {
+
+            subscriptionDict[key] = view[key].subscribe(function (newVal) {
+
+                console.log('update from: ' + key + ' with value: ' + newVal);
+
+                var data = {
+                    client: brain.clientId,
+                    field: key,
+                    value: newVal
+                };
+
+//                history.add(data);
+                connector.send(data);
+
+            });
+
+        } else if (isSelectVar(key)) {
+            subscriptionDict[key] = view[key].subscribe(function (newVal) {
+
+                console.log('update from: ' + key + ' with value: ' + newVal);
+
+                var data = {
+                    client: brain.clientId,
+                    field: key.slice(0, - INPUT_SELECTED_POSTFIX.length)
+                };
+
+                if (newVal) {
+                    connector.lock(data);
+                } else {
+                    connector.unlock(data);
+                }
+            });
+        }
+    });
+
+    connector.connect(URL);
+
+    connector.socket.on('info', function (data) {
+        console.log('new socket info with data: ' + data);
+
+        brain.register(data);
+    });
+
+    connector.socket.on('update', function (data) {
+        console.log('new socket update with data: ' + data);
+
+//        history.add(data);
+
+        view.update(data.field, data.value);
+    });
+
+    connector.socket.on('unlock', function (data) {
+        view.unlock(data.field);
+    });
+
+    connector.socket.on('lock', function (data) {
+        view.lock(data.field);
+    });
+
+});
